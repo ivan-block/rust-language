1e7c0c79fb76168ab4e9301e9af432627fa182df
ivan-block <ivancollev2@gmail.com>
2023-01-31 20:40:36 +0100

    Data types of the Rust language

---
+++ projects/data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/Users/badmanoyinz/rust-language/projects/data_types/target/debug/data_types: /Users/badmanoyinz/rust-language/projects/data_types/src/main.rs
---
+++ projects/data_types/target/debug/deps/data_types-3986844c9aa68aa6.d
@@ -0,0 +1,5 @@
+/Users/badmanoyinz/rust-language/projects/data_types/target/debug/deps/data_types-3986844c9aa68aa6: src/main.rs
+
+/Users/badmanoyinz/rust-language/projects/data_types/target/debug/deps/data_types-3986844c9aa68aa6.d: src/main.rs
+
+src/main.rs:
