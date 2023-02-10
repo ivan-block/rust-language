04d2be9262c2ed96bea6b1d0695d26d3160285ca
ivan-block <ivancollev2@gmail.com>
2023-02-11 00:08:58 +0100

    update the enum, matchand control flow

--- projects/match-control-flow/src/main.rs
+++ projects/match-control-flow/src/main.rs
@@ -26,4 +26,15 @@ fn value_in_cents(coin: Coin) -> u8 {
 
 fn main() {
     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
